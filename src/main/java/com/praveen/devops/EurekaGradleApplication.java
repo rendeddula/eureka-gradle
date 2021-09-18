36193e64602b69d6ac8ae4262f83f1efd03b90e3
Praveen Rendeddula <rpraveen@manh.com>
2021-09-18 18:34:31 +0530

    initial commit

---
+++ src/main/java/com/praveen/devops/EurekaGradleApplication.java
@@ -0,0 +1,15 @@
+package com.praveen.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/devops/EurekaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
